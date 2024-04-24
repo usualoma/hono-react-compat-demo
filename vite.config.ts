8b605e18b83037bb087097032fac11ae6ff19014
Taku Amano <taku@taaas.jp>
2024-04-25 04:51:12 +0900

    Tweaks import aliases in tsconfig.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
 
 export default defineConfig({
   resolve: {
     alias: {
-      'react': path.join(__dirname, 'node_modules', 'hono', 'dist', 'jsx', 'dom'),
-      'react-dom': path.join(__dirname, 'node_modules', 'hono', 'dist', 'jsx', 'dom'),
+      'react': 'hono/jsx/dom',
+      'react-dom': 'hono/jsx/dom',
     },
   },
 })
